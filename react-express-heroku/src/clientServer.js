669543ce0ad7bc078d758cf52d5c50bf0f5c06b2
Anand S <anand.sundar@yasco.in>
2020-09-27 15:33:13 +0530

    Buldable file react app is done

---
+++ react-express-heroku/src/clientServer.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const { createServer } = require('http');
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+
+const normalizerPort = port => parseInt(port, 10);
+const PORT = normalizerPort(process.env.PORT || 3000);
+
+const app = express();
+const dev = app.get('env') !== 'production'
+
+if (!dev) {
+  app.disable('x-powered-by');
+  app.use(compression());
+  app.use(morgan('common'));
+  app.use(express.static(path.resolve(__dirname, '../build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
+  });
+}
+
+if (dev) {
+  app.use(morgan('dev'));
+}
+
+const server = createServer(app);
+
+server.listen(PORT, err => {
+  console.log('err----', err);
+  if (err) {
+    throw err
+  };
+  console.log('server started');
+});
